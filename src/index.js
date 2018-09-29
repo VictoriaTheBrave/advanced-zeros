ba07ad05c1415e07269f4499c1f7405c15a4e0de
Victoria Likhuta <v1ktor1a09051994@gmail.com>
2018-09-29 22:16:40 +0300

    Advanced Zeros/feature: implemented function counting zeros

--- src/index.js
+++ src/index.js
@@ -1,3 +1,48 @@
 module.exports = function getZerosCount(number, base) {
-  // your implementation
+  const primeFactorsArray = findPrimeFactors(base);
+  let resultZerosCount = 0;
+  const arLength = primeFactorsArray.length;
+  if (arLength === 1) {
+    resultZerosCount = findPrimesZerosCount(number, base);
+   } else {
+    let divisor = 1;
+    let primesZerosCount;
+      for (let i = 0; i < arLength; i++) {
+        if (primeFactorsArray[i] === primeFactorsArray[i-1]) {
+          divisor++;
+        } else {
+          divisor = 1;
+        }
+        if (divisor !== 1) {
+          primesZerosCount = Math.floor(findPrimesZerosCount (number, primeFactorsArray[i]) / divisor);
+        } else {
+          primesZerosCount = findPrimesZerosCount (number, primeFactorsArray[i]);
+        }
+        if (i === 0){
+          resultZerosCount = primesZerosCount;
+        } else {
+          resultZerosCount = Math.min(primesZerosCount, resultZerosCount);
+        }
+      }
+    }
+    return resultZerosCount;
+  }
+
+function findPrimeFactors(number) {
+  let primeFactors = [];
+  for (let i = 2; i <= number; i++) {
+    while ((number % i) === 0) {
+      primeFactors.push(i);
+      number = number / i;
+    }
+  }
+  return primeFactors;
+}
+
+function findPrimesZerosCount(number, base) {
+  let count = 0;
+  for (let i = base; number/i >= 1; i = i * base){ 
+    count = count + Math.floor(number/i);
+  }
+  return count; 
 }
